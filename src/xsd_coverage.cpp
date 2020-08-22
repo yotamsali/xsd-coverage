f9a83cbdb236f55e432481a4b9bca4d83c4faa33
yotamsali <yotam.sali@mail.huji.ac.il>
2020-08-22 22:50:36 +0300

    Switching to C++ CMake project

---
+++ src/xsd_coverage.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <tree.h>
+
+int main()
+{
+    cout << "Hello! The program starts in main()."
+         << endl;
+    TreeNode <int> root =
+    Tree<int> t = new Tree()
+    return 0;
+}
